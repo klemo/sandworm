75e6c22df37e33425941fef0912d65c299f516aa
klemo <klemo.vladimir@gmail.com>
2014-11-18 12:48:01 +0100

    basic end-to-end tests using protractor

---
+++ grader/test/e2e/scenarios.js
@@ -0,0 +1,50 @@
+'use strict';
+
+describe('Sandowrm App', function() {
+
+    it('should display login form', function() {
+        browser.get('/');
+        expect(element(by.css('.signin-link')).isDisplayed())
+            .toBe(true);
+    });
+
+    describe('Basic lab list', function() {
+
+        it('should login user', function() {
+            browser.get('/#/');
+            var username = element(
+                by.model('ctrl.user.username'));
+            var password = element(
+                by.model('ctrl.user.password'));
+            username.sendKeys('test');
+            password.sendKeys('test');
+            element(by.css('.btn.btn-primary')).click();
+            expect(element(by.css('.signout-link')).isDisplayed())
+                .toBe(true);
+        });
+
+        it('should display basic labs page', function() {
+            var labList = element.all(by.repeater('lab in labCtrl.labs'));
+            expect(labList.count()).toBe(2);
+        });
+
+        describe('Lab details', function() {
+            
+            beforeEach(function() {
+                browser.driver.get('http://localhost:8080/#/labs/lab-1');
+            });
+
+            it('should display lab details page', function() {    
+                expect(element(by.binding('ctrl.lab.name')).getText()).toBe('LAB1');
+            });
+        });
+
+        it('should sign out current user', function() {
+            browser.driver.get('http://localhost:8080/#/');
+            element(by.css('.signout-link')).click();
+            expect(browser.getCurrentUrl()).toEqual('http://localhost:8080/#/');
+            expect(element(by.css('.signin-link')).isDisplayed())
+                .toBe(true);
+        });
+    });
+});
---
+++ grader/test/protractor_conf.js
@@ -0,0 +1,15 @@
+exports.config = {
+    allScriptsTimeout: 11000,
+    specs: [
+        'e2e/*.js'
+    ],
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    chromeOnly: true,
+    baseUrl: 'http://localhost:8080/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
