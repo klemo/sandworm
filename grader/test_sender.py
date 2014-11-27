a88544fecf00d7fa27c9203af0d0d83e4a760a76
klemo <klemo.vladimir@gmail.com>
2014-11-27 13:55:41 +0100

    some cleaning in mq

--- grader/test_sender.py
+++ grader/test_sender.py
@@ -6,6 +6,9 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 channel.queue_declare(queue=QUEUE)
-channel.basic_publish(exchange='', routing_key=QUEUE, body='Hello World!')
+message = 'hello world'
+properties = pika.BasicProperties(app_id='test_sender',
+                                  content_type='text')
+channel.basic_publish('', QUEUE, message, properties)
 print " [x] Sent 'Hello World!'"
 connection.close()
