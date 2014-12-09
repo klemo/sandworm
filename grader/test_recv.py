2c207892d645d44fa859546a1ba7f21729635563
klemo <klemo.vladimir@gmail.com>
2014-12-09 16:26:51 +0100

    structure around test_recv

--- grader/test_recv.py
+++ grader/test_recv.py
@@ -1,19 +1,42 @@
+#------------------------------------------------------------------------------
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+
 import pika
+import logging
+import simplejson as json
+
+#------------------------------------------------------------------------------
 
 QUEUE = 'sandworm-q-out'
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+#------------------------------------------------------------------------------
 
-channel = connection.channel()
-
-channel.queue_declare(queue=QUEUE, durable=True)
-
-print ' [*] Waiting for messages. To exit press CTRL+C'
-
-def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+def on_message(ch, method, properties, body):
+    try:
+        message = json.loads(body)
+        logging.info('Received archive {} from user {}'.format(
+                message['archive_path'],
+                message['username']))
+    except Exception as e:
+        logging.error(e)
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
-channel.basic_consume(callback, queue=QUEUE)
-channel.start_consuming()
+#------------------------------------------------------------------------------
+
+def consume():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE, durable=True)
+    logging.info('Waiting for messages. To exit press CTRL+C')
+    channel.basic_consume(on_message, queue=QUEUE)
+    channel.start_consuming()
+
+#------------------------------------------------------------------------------
+        
+if __name__=='__main__':
+    logging.getLogger('').handlers = []
+    logging.basicConfig(level=getattr(logging, 'INFO', None),
+                        format='// %(message)s')
+    consume()
